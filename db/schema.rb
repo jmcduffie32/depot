eeff09a2719844fda47e1faf77082a8d144162d6
Jon McDuffie <bobber321988@gmail.com>
2015-01-11 12:21:38 -0500

    Create combin_line_item migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150111164602) do
+ActiveRecord::Schema.define(version: 20150111170427) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
