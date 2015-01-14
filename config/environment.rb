1e0e7ced54715560e75be6ba6ffa63bfbc9c87ee
Jon McDuffie <bobber321988@gmail.com>
2015-01-13 19:26:22 -0500

    Added mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Depot::Application.initialize!
+
+
+Depot::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:          "smtp.gmail.com",
+    port:             587,
+    domain:           "mcduffphysics.com",
+    authentication:   "plain",
+    user_name:        "bobber321988",
+    password:         "GTbreads321",
+    enable_starttls_auto: true
+  }
+end
